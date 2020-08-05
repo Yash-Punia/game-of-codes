4cc7b434e3ecd7e7fcb8c03c8bc35b1b644406d9
Yash-Punia <puniayash@gmail.com>
2020-08-05 12:39:02 +0530

    Added person and coin movement
    Added person script and coin controllers script.
    Also added 2 new scenes for easy work management

---
+++ Assets/CoinController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    private int coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coins = 300;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //coinText.SetText(coins.ToString());
+    }
+
+    public void IncreaseCoins(int amount)
+    {
+        coins += amount;
+    }
+
+    public void DecreaseCoins(int amount)
+    {
+        coins -= amount;
+    }
+
+    public int GetCoins()
+    {
+        return coins;
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minZ;
+    [SerializeField] float maxZ;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float startWaitTime;
+
+    private Animator anim;
+    private Vector3 moveSpot;
+    private float waitTime;
+
+    void Start()
+    {
+        moveSpot = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+        anim = GetComponent<Animator>();
+        waitTime = startWaitTime;
+    }
+
+    void Update()
+    {
+        MovementHandler();
+    }
+
+    private void MovementHandler()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot, moveSpeed * Time.deltaTime);
+        if(moveSpot - transform.position != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveSpot - transform.position);
+        }
+        if (Vector3.Distance(transform.position, moveSpot) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpot = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+                waitTime = startWaitTime;
+                anim.SetBool("Walking", true);
+            }
+            else
+            {
+                anim.SetBool("Walking", false);
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
