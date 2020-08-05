5e8173b11f4e628575e0e62883bbaf550b18bdea
Yash-Punia <puniayash@gmail.com>
2020-08-06 00:04:19 +0530

    Added person
    Updated coin controller
    Added person and person status bars

--- Assets/Scripts/PopulationController.cs
+++ Assets/Scripts/PopulationController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PopulationController : MonoBehaviour
 {
+    [SerializeField] GameObject personPrefab;
+
+    private List<GameObject> people;
+    private Vector3 spawnPoint;
+
+    public int currentPopulation { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPopulation = 0;
+        people = new List<GameObject>();
+        AddPerson();
+        AddPerson();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void AddPerson()
+    {
+        spawnPoint = new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f));
+        GameObject tempPerson = Instantiate(personPrefab, spawnPoint, Quaternion.identity);
+        tempPerson.GetComponent<Person>().personIndex = currentPopulation;
+        currentPopulation++;
+        people.Add(tempPerson);
     }
 }
